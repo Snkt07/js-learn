e78bd1d8e0a1596e4054b4780073c2d5addcea9b
Snkt07 <110517913+Snkt07@users.noreply.github.com>
2025-03-26 08:57:09 +0000

    Updated

---
+++ 01_basics/dT.js
@@ -0,0 +1,16 @@
+let val=null
+let anV=Number(val)
+// console.log(anV)
+// console.log(typeof(anV))
+
+const name="Sanket"
+const newName=new String("Sanketby")
+console.log(`My name is ${name}`);
+console.log(`My updated name is ${newName}`);
+console.log(newName);
+console.log(newName[3]);
+console.log(newName.__proto__);
+console.log(newName.toUpperCase());
+console.log(newName);
+console.log(name.toUpperCase());
+console.log(name);
---
+++ 01_basics/mathNum.js
@@ -0,0 +1,7 @@
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*10 )+ 1)
+
+let min=1
+let max=2
+console.log(Math.floor(Math.random()*(max-min+1))+min)
